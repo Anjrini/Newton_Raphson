ae48d5a018d5f7ec6abc68e04c68634fcead4dd3
Mustafa Anjrini <89596728+Anjrini@users.noreply.github.com>
2025-07-26 20:43:56 +0200

    Add files via upload

---
+++ newton_raphson.R
@@ -0,0 +1,33 @@
+# Mustafa Anjrini on 26.07.2025
+
+rm(list = ls()) # deleting all the variables
+
+newton_raphson<- function(x){ # the input can be a vector of a values in order to get the convergance of different values
+  
+  f<-function(x){ 
+    return(x^3+4*x^2-x-4) # the function to be considered. Please change it accordingly
+  }
+  
+  f_p<-function(x){
+    return(3*x^2+8*x-1) # the derivative of the function to be considered. Please change it accordingly
+  }
+  
+  # below is the implementation of the method
+  for (i in 1:20) {
+    dx<- -f(x)/f_p(x)
+    x<-x+dx
+  }
+  return(x)
+}
+
+# let's give starting values
+x<-c(-10,-2,5)
+
+# applying the function
+newton_raphson(x)
+
+
+
+
+
+
