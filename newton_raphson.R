ff8ca0b86cc40ad9a1af03b4d165da3246d7e0ca
Mustafa Anjrini <89596728+Anjrini@users.noreply.github.com>
2025-07-27 10:54:58 +0200

    Update newton_raphson.R

--- newton_raphson.R
+++ newton_raphson.R
@@ -11,11 +11,23 @@ newton_raphson<- function(x){ # the input can be a vector of a values in order t
   f_p<-function(x){
     return(3*x^2+8*x-1) # the derivative of the function to be considered. Please change it accordingly
   }
-  
   # below is the implementation of the method
-  for (i in 1:20) {
+  for (i in 1:1000) {
     dx<- -f(x)/f_p(x)
+    x.old<-x
     x<-x+dx
+    x.new=x
+    z=0
+    for (i in 1:length(x)) {
+      if (abs(x.new[i]-x.old[i])<0.001) {
+       z=z+1 
+      }
+    }
+    if (z==length(x)) {
+      break
+    }
+    z=0
+    
   }
   return(x)
 }
@@ -25,7 +37,6 @@ x<-c(-10,-2,5)
 
 # applying the function
 newton_raphson(x)
-
 
 
 
